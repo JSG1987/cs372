35d7ef948f48416fd48e868fbea473711f041a6c
JSG1987 <JGentry0058@gmail.com>
2023-05-19 03:30:00 -0500

    added part 2

--- apps/Raw Pointers Are Evil/Project1/Source.cpp
+++ apps/Raw Pointers Are Evil/Project1/Source.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 
 void partuno();
+void partdos();
 
 
 int main()
 {
 	partuno();
+	partdos();
 
 	return 0;
 }
@@ -34,4 +36,25 @@ void partuno()
 
 	}
 	cout << endl;
+}
+
+void partdos()
+{
+	const int arraySize = 1000000;
+	int* ptr1 = new int[arraySize];
+
+	for (int i = 0; i < arraySize; ++i)
+	{
+		ptr1[i] = i;
+	}
+	int* ptr2 = ptr1;
+	delete ptr1;
+
+	std::cout << "Address of ptr2: " << ptr2 << std::endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << ptr2[i] << " ";
+	}
+		cout << endl;
+
 }
