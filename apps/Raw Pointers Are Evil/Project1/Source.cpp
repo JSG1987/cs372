8ef050a7852b10954a0c8cb0dab605fd2f37292f
JSG1987 <JGentry0058@gmail.com>
2023-05-21 01:24:40 -0500

    Cannot run program because of the exceptions. I finished Q1

--- apps/Raw Pointers Are Evil/Project1/Source.cpp
+++ apps/Raw Pointers Are Evil/Project1/Source.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 void partuno();
 void partdos();
+string* allocateString();
+void print(const string* ptr);
 
 
 int main()
 {
 	partuno();
 	partdos();
+	string * strPtr = allocateString();
+	print(strPtr);
 
 	return 0;
 }
@@ -30,6 +34,7 @@ void partuno()
 
 	for (int i = 0; i < 10; i++)
 	{
+		//if (ptr2)
 		cout << ptr2[i] << " ";  //execption thrownException thrown: read access violation.
 		                        //ptr2 was 0x1E64152.
 
@@ -53,8 +58,21 @@ void partdos()
 	std::cout << "Address of ptr2: " << ptr2 << std::endl;
 	for (int i = 0; i < 10; ++i)
 	{
+		//if(ptr2)
 		cout << ptr2[i] << " ";
 	}
 		cout << endl;
 
+}
+
+string* allocateString()
+{
+	string* ptr = new string("abcdefghijkl");
+	return ptr;
+}
+
+void print(const string* ptr)
+{
+	cout << *ptr << endl;
+	delete ptr;
 }
