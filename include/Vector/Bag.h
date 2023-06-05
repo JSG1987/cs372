a9093adf108bc6b489078ba739bf8a64217cef08
JSG1987 <JGentry0058@gmail.com>
2023-06-05 14:57:54 -0500

    BAG

---
+++ include/Vector/Bag.h
@@ -0,0 +1,47 @@
+#pragma once
+//This is the template to question 2 and 3 
+using namespace std;
+#include <vector>
+
+template <typename Thing>
+class Bag {
+
+private :
+	std::vector<Thing> bagVector;
+
+public:
+	 
+	Bag() = default;
+
+	void insert(Thing aThing)
+	{
+		bagVector.push_back(aThing);
+	}
+
+	Thing& pop()
+	{
+		Thing aValue = bagVector.back();
+		bagVector.pop_back();
+		return aValue;
+	}
+	int size()
+	{
+		bagVector.size();
+	}
+	bool isSameThing(Thing a, Thing b)
+	{
+		return(a == b);
+	}
+	int count(Thing &aThing)
+	{
+		count = 0;
+		for (auto anotherThing : bagVector)
+		{
+			if (anotherThing == aThing)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+};
